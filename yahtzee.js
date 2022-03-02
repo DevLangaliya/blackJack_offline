14c6eb2c40b9f6fb9878566fe4d676fbb0cadff6
Dev Langaliya <langaliya.dev@gmail.com>
2022-03-02 00:24:05 -0600

    asdfghjkl

--- yahtzee.js
+++ yahtzee.js
@@ -1,10 +1,16 @@
 let dice = [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]]
 
 function show_dice(){
-    roll = Math.floor(Math.random()*6)
-    console.log(dice[roll], dice[roll][roll])
+    try{
+        roll = Math.floor(Math.random()*6)
+        console.log(dice[roll], dice[roll][roll])
+        document.getElementById("ace").innerHTML = roll
+    }catch(error){
+        document.getElementById("ace").innerHTML = "und"
+    }
 }
 
 function gohome(){
     window.location.href = "http://127.0.0.1:5500/homepage.html"
 }
+
