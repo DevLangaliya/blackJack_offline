55c0c1c923ae48313bb2d7d63d518426655553f2
Dev Langaliya <langaliya.dev@gmail.com>
2022-03-04 09:39:18 -0600

    fghjkl;

--- yahtzee.js
+++ yahtzee.js
@@ -1,15 +1,54 @@
 const dice = [1, 2, 3, 4, 5, 6];
+const p1_upper = [0, 0, 0, 0, 0, 0];
+const p2_upper = [0, 0, 0, 0, 0, 0];
+const p1_lower = [0, 0, 0, 0, 0, 0];
+const p2_lower = [0, 0, 0, 0, 0, 0];
 
 function show_dice(){
-    let dice_arr = []
+    dice_arr = []
     for(let i = 0; i < 5; i++){
         dice_arr.push(dice[Math.floor(Math.random()*dice.length)])
     }
-    curent_hand = dice_arr.join(', ')
-    document.getElementById("dice").innerHTML = curent_hand
+    woah = dice_arr.sort((a,b) => a - b)
+    current_hand = woah.join(', ')
+    document.getElementById("dice").innerHTML = current_hand
+    check_roll()
 }
+
+function check_card(){
+    show_dice()
+    document.getElementById("ace11").innerHTML = woah
+}
+
 
 function gohome(){
     window.location.href = "http://127.0.0.1:5500/homepage.html"
 }
 
+function check_roll(){
+     
+}
+
+function calc_ace(){
+
+}
+
+function calc_two(){
+    
+}
+
+function calc_three(){
+    
+}
+
+function calc_four(){
+    
+}
+
+function calc_five(){
+    
+}
+
+function calc_six(){
+    
+}
